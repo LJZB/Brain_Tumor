88bd9a7246300e0c473e1979d92ec185f6c4a0a9
Luis Zuluaga <luiszuluaga90@gmail.com>
2022-07-05 01:02:15 -0500

    Commit #2 - Detect Feaures Functions added

--- tumores.m
+++ tumores.m
@@ -4,30 +4,67 @@ clear all, close all, clc
 %Carga de la carpeta con imágenes
 brain_tumor_path=imageDatastore('D:\Users\Luis\Documents\MATLAB\tumor\Brain_Tumor_Data_Set\Brain_Tumor\*.*');
 brain_tumor_images = readall(brain_tumor_path);
-
-%% Vista previa de imagen
+%% Preprocesamiento
 % Transformar a escala de grises
-I = brain_tumor_images{1};
-I = rgb2gray(I);
+im = brain_tumor_images{1};
+im_gray = rgb2gray(im);
+figure
+imshow(im_gray);
 
-%% Morfología
-% Otsu Binarization for segmentation
-level = graythresh(I);
-img = im2bw(I,0.26);
-figure, imshow(img);title('Otsu Thresholded Image');
-%%
-I = medfilt2(I);
+% Filtrado de la imagen
+im_filtered=medfilt2(im_gray);
 figure
-imshow(I);
-se = strel('line',11,90);
-I = imdilate(I,se);
-figure
-imshow(I);
+imshow(im_filtered);
+
+%Erosion? Se necesita eliminar lo más posible el borde del cerebro
 %% Detección de características
-mis_puntos = detectSIFTFeatures(I,"EdgeThreshold",10);
-imshow(I);
+%Brisk
+figure
+points = detectBRISKFeatures(im_filtered);
+imshow(im_filtered);
 hold on;
-plot(mis_puntos.selectStrongest(50));
+title('Brisk')
+plot(points.selectStrongest(50));
+
+%Fast
+figure
+points = detectFASTFeatures(im_filtered);
+imshow(im_filtered);
+hold on;
+title('Fast')
+plot(points.selectStrongest(50));
+
+%Harris
+figure
+points = detectHarrisFeatures(im_filtered);
+imshow(im_filtered);
+hold on;
+title('Harris')
+plot(points.selectStrongest(50));
+
+%Kaze
+figure
+points = detectKAZEFeatures(im_filtered);
+imshow(im_filtered);
+hold on;
+title('Kaze')
+plot(points.selectStrongest(50));
+
+%MinEigen
+figure
+points = detectMinEigenFeatures(im_filtered);
+imshow(im_filtered);
+hold on;
+title('MinEigen')
+plot(points.selectStrongest(90));
+
+%MSER
+figure
+regions = detectMSERFeatures(im_filtered);
+imshow(im_filtered);
+hold on;
+title('MSER')
+plot(regions,'showPixelList',true,'showEllipses',false);
 
 %% Extracción de características
 [caracteristicas, puntos_validos] = extractHOGFeatures(I, mis_puntos);
